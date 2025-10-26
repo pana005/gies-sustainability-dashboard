3644812e3d219b39169ab8fd9b20891e51d02449
pana005 <pana005@newschool.edu>
2025-10-26 18:15:52 -0500

    update

--- script.js
+++
@@ -1,9 +0,0 @@
-const container = document.getElementById('container');
-
-// Generate 18 boxes automatically
-for (let i = 1; i <= 18; i++) {
-  const box = document.createElement('div');
-  box.className = 'box';
-  box.textContent = i; // add number label
-  container.appendChild(box);
-}
